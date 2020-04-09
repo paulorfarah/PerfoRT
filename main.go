b985c1daca221f9b551287e4a9a5cbf08fba9680
Paulo Farah <paulo.farah@ospreysecurity.com>
2020-04-09 18:28:19 -0300

    cloning repository in memory

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
 )
 
 func main(){
 	fmt.Println("go-repo-downloader")
+	fmt.Println("git clone https://github.com/paulorfarah/refactoring-python-code")
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: "https://github.com/paulorfarah/refactoring-python-code",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("git log")
+	ref, err := r.Head()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ref)
 }
