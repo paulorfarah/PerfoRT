2317e296efbe579b0ba759ee92304e5560327a1b
Paulo Farah <paulo.farah@ospreysecurity.com>
2020-04-09 19:01:59 -0300

    implemented git log

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
@@ -22,4 +24,19 @@ func main(){
 		fmt.Println(err)
 	}
 	fmt.Println(ref)
+
+	since := time.Date(2019, 12, 31, 0, 0, 0, 0, time.UTC)
+	until := time.Date(2020, 12, 31, 0, 0, 0, 0, time.UTC)
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash(), Since: &since, Until: &until})
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = cIter.ForEach(func(c *object.Commit) error {
+		fmt.Println(c)
+
+		return nil
+	})
+	if err != nil{
+		fmt.Println(err)
+	}
 }
