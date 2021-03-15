470379e97d03104cfd55e987eadbf750926418bb
Paulo Farah <paulorfarah@gmail.com>
2021-03-15 20:18:40 -0300

    trying shell script to fix randoop error

--- runner.go
+++ runner.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	fmt.Println("teste")
-	cmd := exec.Command("java", "-classpath", "/mnt/sda4/go-work/src/github.com/paulorfarah/repos/TestProject/target/classes:$RANDOOP_JAR", "randoop.main.Main", "gentests", "--testclass=testproject.Test")
+	// cmd := exec.Command("java", "-classpath", "/mnt/sda4/go-work/src/github.com/paulorfarah/repos/TestProject/target/classes:$RANDOOP_JAR", "randoop.main.Main", "gentests", "--testclass=testproject.Test")
+	cmd := exec.Command("randoop.sh")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
