df1a7db41c694302e8eeb3ecc98d1fc1ff57a7b6
Paulo Farah <paulorfarah@gmail.com>
2021-03-15 15:40:57 -0300

    test randoop command, added error message

--- runner.go
+++ runner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 )
@@ -8,13 +9,17 @@ import (
 func main() {
 	fmt.Println("teste")
 	cmd := exec.Command("java", "-classpath", "/mnt/sda4/go-work/src/github.com/paulorfarah/repos/TestProject/target/classes:$RANDOOP_JAR", "randoop.main.Main", "gentests", "--testclass=testproject.Test")
-	out, err := cmd.Output()
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
 	if err != nil {
-		fmt.Println("\n[>>ERROR]: Cannot run randoop gentests: ", err)
+		fmt.Println("\n[>>ERROR]: Cannot run randoop gentests (" + fmt.Sprint(err) + "): " + stderr.String())
 		fmt.Println(out)
 	} else {
 		fmt.Println("\n [>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SUCCESS]: Randoop executed successully!")
-		fmt.Println(out)
+		fmt.Println(out.String())
 
 	}
 }
