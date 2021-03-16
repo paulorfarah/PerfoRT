e30cf1d8fe5f60fb913d116d7f77a0ba15d5c1cc
Paulo Farah <paulorfarah@gmail.com>
2021-03-16 07:47:37 -0300

    create sh script file to run randoop

--- runner.go
+++ runner.go
@@ -3,13 +3,18 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	fmt.Println("teste")
 	// cmd := exec.Command("java", "-classpath", "/mnt/sda4/go-work/src/github.com/paulorfarah/repos/TestProject/target/classes:$RANDOOP_JAR", "randoop.main.Main", "gentests", "--testclass=testproject.Test")
-	cmd := exec.Command("randoop.sh")
+	script := CreateRandoopScript("testproject.Test")
+
+	cmd := exec.Command(script)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
@@ -23,4 +28,31 @@ func main() {
 		fmt.Println(out.String())
 
 	}
+}
+
+func CreateRandoopScript(class string) string {
+	f := strings.ReplaceAll(class, ".", "_") + ".sh"
+	// Create new file
+	new, err := os.Create(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer new.Close()
+
+	stats, err := os.Stat(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Permission File Before: %s\n", stats.Mode())
+	err = os.Chmod(f, 0700)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stats, err = os.Stat(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Permission File After: %s\n", stats.Mode())
+	return f
 }
