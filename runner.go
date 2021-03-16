5067a211a6e8d79dc7bf2dc232f579961482a4ea
Paulo Farah <paulorfarah@gmail.com>
2021-03-16 08:51:33 -0300

    fix sh script to run randoop

--- runner.go
+++ runner.go
@@ -14,7 +14,7 @@ func main() {
 	// cmd := exec.Command("java", "-classpath", "/mnt/sda4/go-work/src/github.com/paulorfarah/repos/TestProject/target/classes:$RANDOOP_JAR", "randoop.main.Main", "gentests", "--testclass=testproject.Test")
 	script := CreateRandoopScript("testproject.Test")
 
-	cmd := exec.Command("./" + script)
+	cmd := exec.Command("bash " + script)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
