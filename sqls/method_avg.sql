075719ae263e849f5f7d1161cc44e98d01683fe7
Paulo R. Farah <paulorfarah@gmail.com>
2022-08-16 11:02:11 -0300

    added database name to method_avg.sql

--- sqls/method_avg.sql
+++ sqls/method_avg.sql
@@ -1,3 +1,4 @@
+USE perfrt;
 SELECT repo.name, mea.id AS measurement, c.committer_date, commit_hash, test.name AS test_name, r.id AS run, f.name AS class_name, m.name AS method_name, m.created_at AS method_started_at, m.ended_at AS method_ended_at, m.caller_id, m.own_duration, m.cumulative_duration, 
 AVG(res.cpu_percent), STD(res.cpu_percent)
 FROM commits AS c
