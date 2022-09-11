dda1bae76d2f109d8cfe61a004d038afe7426a9d
Paulo R. Farah <paulorfarah@gmail.com>
2022-09-11 19:09:50 -0300

    cleaned pomparser code

--- pomparser.go
+++ pomparser.go
@@ -337,33 +337,15 @@ type ActivationFile struct {
 }
 
 func ParsePom(file string) (Project, error) {
-	// fmt.Println("pom.xml parser..")
+	var project Project
 
-	// Open our xmlFile
 	xmlFile, err := os.Open(file)
-	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	fmt.Println("Successfully Opened users.xml")
-	// defer the closing of our xmlFile so that we can parse it later on
 	defer xmlFile.Close()
-	// read our opened xmlFile as a byte array.
+
 	byteValue, _ := ioutil.ReadAll(xmlFile)
-
-	// we initialize our Users array
-	var project Project
-	// we unmarshal our byteArray which contains our
-	// xmlFiles content into 'users' which we defined above
 	xml.Unmarshal(byteValue, &project)
-
-	// for i := 0; i < len(project.Build.BuildBase.Plugins); i++ {
-	// 	fmt.Println("ArtifactID: " + project.Build.BuildBase.Plugins[i].ArtifactID)
-	// 	fmt.Println("Source: " + project.Build.BuildBase.Plugins[i].Configuration.Source)
-	// 	fmt.Println("Target: " + project.Build.BuildBase.Plugins[i].Configuration.Target)
-	// 	// fmt.Println("User Name: " + users.Users[i].Name)
-	// 	// fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
-	// }
 	return project, err
 }
